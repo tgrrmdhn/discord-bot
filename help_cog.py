e849ea245ea7eecd6bf47383d81914594bfd0164
tgrrmdhn Workspace <101447471+tgrrmdhn@users.noreply.github.com>
2024-02-22 18:51:44 +0700

    Update help_cog.py

--- help_cog.py
+++ help_cog.py
@@ -18,7 +18,7 @@ class help_cog(commands.Cog):
         self.help_embed.add_field(name=f"{self.bot.command_prefix}8ball <question>", value="Magic 8-Ball responses", inline=False)
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.bot.change_presence(status=discord.Status(f"/help or {self.bot.command_prefix}help | by @asianpingissue"))
+        await self.bot.change_presence(activity=discord.Game(f"/help or {self.bot.command_prefix}help | by @asianpingissue"))
     @commands.hybrid_command()
     async def help(self, ctx):
         await ctx.send(embed=self.help_embed)
